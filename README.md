3f6123501b875f9b7381227b3e53fd882140b1d8
assuerogames <114956202+assuerogames@users.noreply.github.com>
2024-06-12 12:13:40 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+by: Dário Assuero
+
+Você pode instalar a biblioteca "pug" usando o pip:
+
+pip install pug
---
+++ pug/__init__.py
@@ -0,0 +1,1 @@
+from .core import greet, auto_click, auto_write, show_mouse_position, press_enter, wait, pug_cat, move_mouse, right_click, press_key, screenshot, get_screen_resolution, wait_for_image
